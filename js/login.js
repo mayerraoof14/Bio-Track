17ba53824c7d387d83585563b8e374180480777b
mayerraoof14 <mayerraoof14@gmail.com>
2025-04-25 13:23:54 +0300

    login.js

--- js/login.js
+++ js/login.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         loginButton.disabled = true;
 
-        fetch('./login.php', {
+        fetch('/login.php', {
             method: 'POST',
             body: formData
         })
